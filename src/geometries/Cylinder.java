ef3f18051c1717cd94615047c68363908a1e4844
User <ylutvak@gmail.com>
2020-03-19 16:45:27 +0200

    19/03/2020 16:30
    Signed-off-by: User <ylutvak@gmail.com>

--- src/geometries/Cylinder.java
+++ src/geometries/Cylinder.java
@@ -5,20 +5,17 @@ import primitives.Ray;
 import primitives.Vector;
 
 public class Cylinder extends RadialGeometry {
-    Point3D CenterBottom;
     Ray ray;
     double height;
 
-    public Cylinder(double _radius, Point3D centerBottom, Ray ray, double height) {
+    public Cylinder(double _radius, Ray ray, double height) {
         super(_radius);
-        CenterBottom = centerBottom;
         this.ray = ray;
         this.height = height;
     }
 
-    public Cylinder(RadialGeometry _rg, Point3D centerBottom, Ray ray, double height) {
+    public Cylinder(RadialGeometry _rg, Ray ray, double height) {
         super(_rg);
-        CenterBottom = centerBottom;
         this.ray = ray;
         this.height = height;
     }
---
+++ src/geometries/Main.java
@@ -0,0 +1,74 @@
+package geometries;
+
+import primitives.*;
+import static java.lang.System.out;
+import static primitives.Util.*;
+
+/**
+ * Test program for the 1st stage
+ *
+ * @author Dan Zilberstein
+ */
+public final class Main {
+
+    /**
+     * Main program to tests initial functionality of the 1st stage
+     *
+     * @param args irrelevant here
+     */
+    public static void main(String[] args) {
+
+        try { // test zero vector
+            new Vector(0, 0, 0);
+            out.println("ERROR: zero vector does not throw an exception");
+        } catch (Exception e) {}
+
+        Vector v1 = new Vector(1, 2, 3);
+        Vector v2 = new Vector(-2, -4, -6);
+        Vector v3 = new Vector(0, 3, -2);
+
+        // test length..
+        if (!isZero(v1.lengthSquared() - 14))
+            out.println("ERROR: lengthSquared() wrong value");
+        if (!isZero(new Vector(0, 3, 4).length() - 5))
+            out.println("ERROR: length() wrong value");
+
+        // test Dot-Product
+        if (!isZero(v1.dotProduct(v3)))
+            out.println("ERROR: dotProduct() for orthogonal vectors is not zero");
+        if (!isZero(v1.dotProduct(v2) + 28))
+            out.println("ERROR: dotProduct() wrong value");
+
+        // test Cross-Product
+        try { // test zero vector
+            v1.crossProduct(v2);
+            out.println("ERROR: crossProduct() for parallel vectors does not throw an exception");
+        } catch (Exception e) {}
+        Vector vr = v1.crossProduct(v3);
+        if (!isZero(vr.length() - v1.length() * v3.length()))
+            out.println("ERROR: crossProduct() wrong result length");
+        if (!isZero(vr.dotProduct(v1)) || !isZero(vr.dotProduct(v3)))
+            out.println("ERROR: crossProduct() result is not orthogonal to its operands");
+
+        // test vector normalization vs vector length and cross-product
+        Vector v = new Vector(1, 2, 3);
+        Vector vCopy = new Vector(v);
+        Vector vCopyNormalize = vCopy.normalize();
+        if (vCopy != vCopyNormalize)
+            out.println("ERROR: normalize() function creates a new vector");
+        if (!isZero(vCopyNormalize.length() - 1))
+            out.println("ERROR: normalize() result is not a unit vector");
+        Vector u = v.normalized();
+        if (u == v)
+            out.println("ERROR: normalizated() function does not create a new vector");
+
+        // Test operations with points and vectors
+        Point3D p1 = new Point3D(1, 2, 3);
+        if (!Point3D.ZERO.equals(p1.add(new Vector(-1, -2, -3))))
+            out.println("ERROR: Point + Vector does not work correctly");
+        if (!new Vector(1, 1, 1).equals(new Point3D(2, 3, 4).subtract(p1)))
+            out.println("ERROR: Point - Point does not work correctly");
+
+        out.println("If there were no any other outputs - all tests succeeded!");
+    }
+}
