2128569e37a6f0fa0d0dd35e941cbe26f2b72701
User <ylutvak@gmail.com>
2020-03-25 22:02:05 +0200

    25/03/2020 22:00
    Signed-off-by: User <ylutvak@gmail.com>

---
+++ UnitTests/UnitTests/SphereTest.java
@@ -0,0 +1,20 @@
+package UnitTests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SphereTest {
+
+    @Test
+    public void getCenter() {
+    }
+
+    @Test
+    public void getNormal() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+}
