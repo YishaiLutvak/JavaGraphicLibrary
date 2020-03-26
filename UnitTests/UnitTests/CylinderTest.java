57cbffd66219eb2d44890cd56561e360f8d5cdf2
User <ylutvak@gmail.com>
2020-03-26 12:06:00 +0200

    add tests including Cylinder
    Signed-off-by: User <ylutvak@gmail.com>

---
+++ UnitTests/UnitTests/CylinderTest.java
@@ -0,0 +1,20 @@
+package UnitTests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CylinderTest {
+
+    @Test
+    public void get_height() {
+    }
+
+    @Test
+    public void getNormal() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+}
