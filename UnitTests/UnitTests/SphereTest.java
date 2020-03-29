f1df0beaadd868014589aee3892f216aa0f53215
User <ylutvak@gmail.com>
2020-03-29 20:33:24 +0300

    add testing Sphare 29/03/2020 18:15
    Signed-off-by: User <ylutvak@gmail.com>

--- UnitTests/UnitTests/SphereTest.java
+++ UnitTests/UnitTests/SphereTest.java
@@ -1,6 +1,9 @@
 package UnitTests;
 
+import geometries.Sphere;
 import org.junit.Test;
+import primitives.Point3D;
+import primitives.Vector;
 
 import static org.junit.Assert.*;
 
@@ -11,14 +14,17 @@ import static org.junit.Assert.*;
 public class SphereTest {
 
     @Test
-    public void getCenter() {
-        // ============ Equivalence Partitions Tests ==============
-        // =============== Boundary Values Tests ==================
-    }
-
-    @Test
     public void getNormal() {
+        Sphere s1 = new Sphere(5, new Point3D(0,0,0));
+
         // ============ Equivalence Partitions Tests ==============
-        // =============== Boundary Values Tests ==================
+
+        assertTrue(s1.getNormal(new Point3D(0,0,5)).equals(new Vector(new Point3D(0,0,1))));
+        assertTrue(s1.getNormal(new Point3D(0,0,-5)).equals(new Vector(new Point3D(0,0,-1))));
+        assertTrue(s1.getNormal(new Point3D(0,5,0)).equals(new Vector(new Point3D(0,1,0))));
+        assertTrue(s1.getNormal(new Point3D(0,-5,0)).equals(new Vector(new Point3D(0,-1,0))));
+        assertTrue(s1.getNormal(new Point3D(5,0,0)).equals(new Vector(new Point3D(1,0,0))));
+        assertTrue(s1.getNormal(new Point3D(-5,0,0)).equals(new Vector(new Point3D(-1,0,0))));
+
     }
 }
--- src/primitives/Ray.java
+++ src/primitives/Ray.java
@@ -75,27 +75,3 @@ public class Ray {
                 ", direction= " + _direction;
     }
 }
-
-//public class SphereTests {
-//
-//    @Test
-//    public void getNormalTest() {
-//        Sphere s1 = new Sphere(4, new Point3D(0,0,0));
-//        Sphere s2 = new Sphere(1, new Point3D(1,1,1));
-//
-//
-//        assertTrue(s1.getNormal(new Point3D(0,0,4)).equals(new Vector(new Point3D(0,0,1))));
-//        assertTrue(s1.getNormal(new Point3D(0,0,-4)).equals(new Vector(new Point3D(0,0,-1))));
-//        assertTrue(s1.getNormal(new Point3D(0,4,0)).equals(new Vector(new Point3D(0,1,0))));
-//        assertTrue(s1.getNormal(new Point3D(0,-4,0)).equals(new Vector(new Point3D(0,-1,0))));
-//        assertTrue(s1.getNormal(new Point3D(4,0,0)).equals(new Vector(new Point3D(1,0,0))));
-//        assertTrue(s1.getNormal(new Point3D(-4,0,0)).equals(new Vector(new Point3D(-1,0,0))));
-//
-//        assertTrue(s2.getNormal(new Point3D(1,1,0)).equals(new Vector(new Point3D(0,0,-1))));
-//        assertTrue(s2.getNormal(new Point3D(0,1,1)).equals(new Vector(new Point3D(-1,0,0))));
-//        assertTrue(s2.getNormal(new Point3D(1,0,1)).equals(new Vector(new Point3D(0,-1,0))));
-//
-//        assertEquals(s1.getNormal(new Point3D(7,7,7)),null);
-//
-//    }
-//}
