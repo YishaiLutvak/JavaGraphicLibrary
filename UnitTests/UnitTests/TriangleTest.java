b3e9e82ba92172bec6c112167b64fff15db3139b
michael <michael155750@gmail.com>
2020-04-19 00:08:27 +0300

    הגדרתי את הטסטים של המשולש בלי מימוש

--- UnitTests/UnitTests/TriangleTest.java
+++ UnitTests/UnitTests/TriangleTest.java
@@ -3,6 +3,7 @@ package UnitTests;
 import geometries.Triangle;
 import org.junit.Test;
 import primitives.Point3D;
+import primitives.Ray;
 import primitives.Vector;
 import static org.junit.Assert.*;
 
@@ -30,5 +31,36 @@ public class TriangleTest {
 
     @Test
     public void findIntersections() {
+        Triangle triangle = new Triangle(new Point3D(2,0,1),new Point3D(-2,0,1),new Point3D(0,2,1));
+
+        // ============ Equivalence Partitions Tests ==============
+
+        //T01: Ray's line don't cross the triangle(0 points)
+        assertEquals("Ray's line out of plane", null,
+                triangle.findIntersections(new Ray(new Point3D(0, 2, 0), new Vector(1, 1, 1))));
+        //T02: Ray starts before the triangle(1 points)
+        //T03: Ray starts after the triangle(0 points)
+
+        // =============== Boundary Values Tests ==================
+
+
+        //T11: Ray starts on the plane and not orthogonl(0 points)
+
+        //ray starts on the plane of the triangle outside the triangle
+        ///ray is not orthogonal to the plane and not contained in the plane
+
+        //*******group: ray is orthgonal to the triangle********
+        //ray starts before the triangle(1 points)
+        //T11: Ray starts on the triangle(0 points)
+        //T11: Ray starts after the triangle(0 points)
+
+        //********group: ray is orthogonal to the plane of the triangle***********
+        //ray starts before the plane(0 points)
+        //T11: Ray starts on the plane(0 points)
+        //T11: Ray starts after the plane(0 points)
+
+        //******group: ray is contained in the plane*******
+        //the triangle contain the ray
+        //the triangle don't contain the ray
     }
 }
