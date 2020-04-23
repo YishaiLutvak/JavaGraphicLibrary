fa1033a34b12196df305f787d3129806c2138383
michael <michael155750@gmail.com>
2020-04-23 08:39:58 +0300

    סוף בניית השלד

--- UnitTests/UnitTests/CameraIntersectionTest.java
+++ UnitTests/UnitTests/CameraIntersectionTest.java
@@ -2,10 +2,22 @@ package UnitTests;
 
 import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
 class CameraIntersectionTest {
     @Test
-    
+    public void testIntegrationForTriangle() {
+
+    }
+
+    @Test
+    public void testIntegrationForSphere() {
+
+    }
+
+    @Test
+    public void testIntegrationForPlane() {
+
+    }
 
 }
--- src/elements/Camera.java
+++ src/elements/Camera.java
@@ -5,12 +5,25 @@ import primitives.Ray;
 import primitives.Vector;
 
 
+/**
+ * represent a camera that photo 3D geometries
+ * @author Yishai Lutvak and Michael Bergshtein
+ */
+
 public class Camera {
+
     protected Point3D location;
     protected Vector vup;
     protected Vector vright;
     protected Vector vto;
 
+    /**
+     * Constract a camera by two vectors and location. Calculate the thirs vector
+     * @param location the  location of the camera
+     *
+     * @param vto vector from the camera to the geometry
+     * @param vup vector from the camera up. orhogonal to vto
+     */
     public Camera(Point3D location, Vector vup, Vector vto) {
         this.location = location;
         this.vup = vup.normalize();
@@ -21,7 +34,7 @@ public class Camera {
 
 
     }
-
+/**********getters**********/
     public Point3D getLocation() {
         return location;
     }
@@ -37,6 +50,18 @@ public class Camera {
     public Vector getVto() {
         return vto;
     }
+
+    /**
+     *
+     * @param nX
+     * @param nY
+     * @param j
+     * @param i
+     * @param screenDistance
+     * @param screenWidth
+     * @param screenHeight
+     * @return
+     */
     public Ray constructRayThroughPixel (int nX, int nY,
                                          int j, int i, double screenDistance,
                                          double screenWidth, double screenHeight){
