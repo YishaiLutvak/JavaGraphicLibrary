1b5f5af109406d892beaed2d14aac0783a866415
User <ylutvak@gmail.com>
2020-05-03 17:19:36 +0300

    after complete stage 4 (2) 03/05/2020 17:20
    Signed-off-by: User <ylutvak@gmail.com>

--- UnitTests/UnitTests/IntegrationTest.java
+++ UnitTests/UnitTests/IntegrationTest.java
@@ -18,6 +18,7 @@ import static org.junit.Assert.*;
 /**
  * Testing integration of camera and intersection
  * in sphere, plane and triangle
+ *
  * @author Michael Bergshtein and Yishai Lutvak
  */
 public class IntegrationTest {
@@ -25,13 +26,13 @@ public class IntegrationTest {
     //camera for testing
     private Camera camera1 = new Camera(
             Point3D.ZERO,
-            new Vector(0, 0,1),
-            new Vector(0,-1,0));
+            new Vector(0, 0, 1),
+            new Vector(0, -1, 0));
 
-   private Camera camera2 = new Camera(
-            new Point3D(0,0,-0.5),
-            new Vector(0, 0,1),
-            new Vector(0,-1,0));
+    private Camera camera2 = new Camera(
+            new Point3D(0, 0, -0.5),
+            new Vector(0, 0, 1),
+            new Vector(0, -1, 0));
 
     //list of results for camera1 and camera2
     List<Ray> rays1 = new ArrayList<Ray>();
@@ -42,16 +43,16 @@ public class IntegrationTest {
      * construct the rays for camera1 (rays1) and the rays for camera2 (rays2)
      */
     public IntegrationTest() {
-        for (int i = 0; i < 3;i++)
-            for (int j = 0; j < 3;j++)
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
                 rays1.add(camera1.constructRayThroughPixel(
-                        3,3, j, i, 1,3,3));
+                        3, 3, j, i, 1, 3, 3));
 
 
-        for (int i = 0; i < 3;i++)
-            for (int j = 0; j < 3;j++)
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
                 rays2.add(camera2.constructRayThroughPixel(
-                        3,3, j, i, 1,3,3));
+                        3, 3, j, i, 1, 3, 3));
     }
 
     /**
@@ -68,14 +69,14 @@ public class IntegrationTest {
 
         Sphere sphere1 = new Sphere(
                 1,
-                new Point3D(0,0,3));
+                new Point3D(0, 0, 3));
 
         for (Ray ray : rays1) {
             List<Point3D> tempIntersections = sphere1.findIntersections(ray);
             if (tempIntersections != null)
-                sumIntersections+= tempIntersections.size();
+                sumIntersections += tempIntersections.size();
         }
-        assertEquals("Wrong number of points",2,sumIntersections);
+        assertEquals("Wrong number of points", 2, sumIntersections);
 
 
         // TC02: 3X3 center: (0,0,2.5) radius:2.5
@@ -83,59 +84,59 @@ public class IntegrationTest {
 
         Sphere sphere2 = new Sphere(
                 2.5,
-                new Point3D(0,0,2.5));
+                new Point3D(0, 0, 2.5));
 
         sumIntersections = 0;
         for (Ray ray : rays2) {
             List<Point3D> tempIntersections = sphere2.findIntersections(ray);
             if (tempIntersections != null)
-                sumIntersections+= tempIntersections.size();
+                sumIntersections += tempIntersections.size();
         }
-        assertEquals("Wrong number of points",18,sumIntersections);
+        assertEquals("Wrong number of points", 18, sumIntersections);
 
 
         // TC03: 3X3 center: (0,0,2) radius:2
         // the plane inside the sphere but the camera outside the sphere
         Sphere sphere3 = new Sphere(
                 2,
-                new Point3D(0,0,2));
+                new Point3D(0, 0, 2));
 
         sumIntersections = 0;
         for (Ray ray : rays2) {
             List<Point3D> tempIntersections = sphere3.findIntersections(ray);
             if (tempIntersections != null)
-                sumIntersections+= tempIntersections.size();
+                sumIntersections += tempIntersections.size();
         }
-        assertEquals("Wrong number of points",10,sumIntersections);
+        assertEquals("Wrong number of points", 10, sumIntersections);
 
         // TC04: 3X3 center: (0,0,1) radius:4
         // the plane and the camera inside the sphere
         Sphere sphere4 = new Sphere(
                 4,
-                new Point3D(0,0,1));
+                new Point3D(0, 0, 1));
 
         sumIntersections = 0;
         for (Ray ray : rays1) {
             List<Point3D> tempIntersections = sphere4.findIntersections(ray);
             if (tempIntersections != null)
-                sumIntersections+= tempIntersections.size();
+                sumIntersections += tempIntersections.size();
         }
-        assertEquals("Wrong number of points",9,sumIntersections);
+        assertEquals("Wrong number of points", 9, sumIntersections);
 
 
         // TC05: 3X3 center: (0,0,-1) radius:0/5
         // the sphere behind the camera
         Sphere sphere5 = new Sphere(
                 0.5,
-                new Point3D(0,0,-1));
+                new Point3D(0, 0, -1));
 
         sumIntersections = 0;
         for (Ray ray : rays1) {
             List<Point3D> tempIntersections = sphere5.findIntersections(ray);
             if (tempIntersections != null)
-                sumIntersections+= tempIntersections.size();
+                sumIntersections += tempIntersections.size();
         }
-        assertEquals("Wrong number of points",0,sumIntersections);
+        assertEquals("Wrong number of points", 0, sumIntersections);
     }
 
     /**
@@ -147,51 +148,51 @@ public class IntegrationTest {
     public void CameraPlaneIntersections() {
         // TC11: 3X3
         // the plane is parallel to the view plane
-        Plane plane1 = new Plane(new Point3D(0,0,2),new Vector(camera1.getVto()));
+        Plane plane1 = new Plane(new Point3D(0, 0, 2), new Vector(camera1.getVto()));
 
         sumIntersections = 0;
         for (Ray ray : rays1) {
             List<Point3D> tempIntersections = plane1.findIntersections(ray);
             if (tempIntersections != null)
-                sumIntersections+= tempIntersections.size();
+                sumIntersections += tempIntersections.size();
         }
-        assertEquals("Wrong number of points",9,sumIntersections);
+        assertEquals("Wrong number of points", 9, sumIntersections);
 
         // TC12: 3X3
         // the plane is not parallel to the view plane - three intersection points
-        Plane plane2 = new Plane(new Point3D(0,0,5),new Vector(0,0.5,1));
+        Plane plane2 = new Plane(new Point3D(0, 0, 5), new Vector(0, 0.5, 1));
 
         sumIntersections = 0;
         for (Ray ray : rays1) {
             List<Point3D> tempIntersections = plane2.findIntersections(ray);
             if (tempIntersections != null)
-                sumIntersections+= tempIntersections.size();
+                sumIntersections += tempIntersections.size();
         }
-        assertEquals("Wrong number of points",9,sumIntersections);
+        assertEquals("Wrong number of points", 9, sumIntersections);
 
         // TC13: 3X3
         // the plane is not parallel to the view plane - two intersection points
-        Plane plane3 = new Plane(new Point3D(0,0,5),new Vector(0,1,1));
+        Plane plane3 = new Plane(new Point3D(0, 0, 5), new Vector(0, 1, 1));
 
         sumIntersections = 0;
         for (Ray ray : rays1) {
             List<Point3D> tempIntersections = plane3.findIntersections(ray);
             if (tempIntersections != null)
-                sumIntersections+= tempIntersections.size();
+                sumIntersections += tempIntersections.size();
         }
-        assertEquals("Wrong number of points",6,sumIntersections);
+        assertEquals("Wrong number of points", 6, sumIntersections);
 
         // TC14: 3X3
         // the plane is orthogonal to the view plane in pCenter point
-        Plane plane4 = new Plane(new Point3D(0,0,2),new Vector(camera1.getVup()));
+        Plane plane4 = new Plane(new Point3D(0, 0, 2), new Vector(camera1.getVup()));
 
         sumIntersections = 0;
         for (Ray ray : rays1) {
             List<Point3D> tempIntersections = plane4.findIntersections(ray);
             if (tempIntersections != null)
-                sumIntersections+= tempIntersections.size();
+                sumIntersections += tempIntersections.size();
         }
-        assertEquals("Wrong number of points",0,sumIntersections);
+        assertEquals("Wrong number of points", 0, sumIntersections);
     }
 
     /**
@@ -212,9 +213,9 @@ public class IntegrationTest {
         for (Ray ray : rays1) {
             List<Point3D> tempIntersections = triangle1.findIntersections(ray);
             if (tempIntersections != null)
-                sumIntersections+= tempIntersections.size();
+                sumIntersections += tempIntersections.size();
         }
-        assertEquals("Wrong number of points",1,sumIntersections);
+        assertEquals("Wrong number of points", 1, sumIntersections);
 
         // TC22: 3X3
         // the triangle is parallel to the view plane - two intersection point
@@ -227,8 +228,8 @@ public class IntegrationTest {
         for (Ray ray : rays1) {
             List<Point3D> tempIntersections = triangle2.findIntersections(ray);
             if (tempIntersections != null)
-                sumIntersections+= tempIntersections.size();
+                sumIntersections += tempIntersections.size();
         }
-        assertEquals("Wrong number of points",2,sumIntersections);
+        assertEquals("Wrong number of points", 2, sumIntersections);
     }
 }
