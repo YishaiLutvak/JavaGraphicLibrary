3590df2195da8c26800eb5dd7f76d3d95d59ea13
User <ylutvak@gmail.com>
2020-05-19 23:58:28 +0300

    finish two tests 19/05/2020 16:25
    Signed-off-by: User <ylutvak@gmail.com>

--- UnitTests/UnitTests/LightsTests.java
+++ UnitTests/UnitTests/LightsTests.java
@@ -182,9 +182,19 @@ public class LightsTests {
                 new Sphere(new Color(java.awt.Color.BLUE), new Material(0.5, 0.5, 100), 50, new Point3D(0, 0, 50)));
 
         scene.addLights(
-                new SpotLight(new Color(500, 300, 0), new Point3D(-50, 50, -50), new Vector(1, -1, 2), 1, 0.00001, 0.00000001),
-                new DirectionalLight(new Color(500, 300, 0), new Vector(1, -1, 1)),
-                new PointLight(new Color(500, 300, 0), new Point3D(-50, 50, -50), 1, 0.00001, 0.000001));
+                new DirectionalLight(
+                        new Color(0, 300, 0),
+                        new Vector(1, 0, 1)),
+                new SpotLight(
+                        new Color(300, 0, 0),
+                        new Point3D(0, 70, -0),
+                        new Vector(0, -1, 0),
+                        1, 0.0001, 0.000005),
+                new PointLight(
+                        new Color(500, 300, 0),
+                        new Point3D(-30, 30, -30),
+                        1, 0.00001, 0.000001));
+
 
         ImageWriter imageWriter = new ImageWriter("sphereAllLights", 150, 150, 500, 500);
         Render render = new Render(imageWriter, scene);
@@ -211,9 +221,19 @@ public class LightsTests {
                         new Point3D(-150, 150, 150), new Point3D(-70, -70, 50), new Point3D(75, -75, 150)));
 
         scene.addLights(
-                new DirectionalLight(new Color(15, 15, 15), new Vector(0, 0, 1)),
-                new SpotLight(new Color(200, 20, 20), new Point3D(-50, 50, -50), new Vector(1, -1, 2), 1, 0.00001, 0.00000001),
-                new PointLight(new Color(0, 0, 0), new Point3D(50, 50, -50), 1, 0.00001, 0.000001));
+                new DirectionalLight(
+                        new Color(180, 50, 50),
+                        new Vector(-1, -1, 1)),
+                new SpotLight(
+                        new Color(50, 50, 300),
+                        new Point3D(0, 0, 115),
+                        new Vector(-1, 0, 2),
+                        1, 0.0001, 0.000005),
+                new PointLight(
+                        new Color(300, 300, 250),
+                        new Point3D(0, 0, 125),
+                        1, 0.0005, 0.0005)
+        );
 
         ImageWriter imageWriter = new ImageWriter("trianglesAllLights", 200, 200, 500, 500);
         Render render = new Render(imageWriter, scene);
