4a4037a6487d5ccd96c2b8414147f9b7884362e5
User <ylutvak@gmail.com>
2020-05-24 14:12:37 +0300

    together stage 7 (6) 24/05/2020 14:10
    Signed-off-by: User <ylutvak@gmail.com>

--- src/renderer/Render.java
+++ src/renderer/Render.java
@@ -184,7 +184,8 @@ public class Render {
      * @return the color with specular effect in the point
      */
     private Color calcSpecular(double ks, Vector l, Vector n, Vector v, int nShininess, Color lightIntensity) {
-        Vector r = l.add(n.scale(-2*l.dotProduct(n)));
+        /*Vector r = l.add(n.scale(-2*l.dotProduct(n)));*/
+        Vector r = reflectionDirection(l,n);
         return new Color(lightIntensity.scale(ks * Math.pow(v.scale(-1).dotProduct(r), nShininess)));
     }
 
@@ -232,8 +233,9 @@ public class Render {
     private boolean unshaded(LightSource light ,Vector l, Vector n, GeoPoint gp){
         Vector lightDirection = l.scale(-1); // from point to light source
 
-        Vector delta = n.scale(n.dotProduct(lightDirection) > 0 ? DELTA : - DELTA);
-        Point3D point = gp._point.add(delta);
+       /* Vector delta = n.scale(n.dotProduct(lightDirection) > 0 ? DELTA : - DELTA);
+        Point3D point = gp._point.add(delta);*/
+        Point3D point = moveDelta(gp._point,lightDirection,n);
         Ray lightRay = new Ray(point, lightDirection);
 
         List<GeoPoint> intersections = _scene.getGeometries().findIntersections(lightRay,light.getDistance(gp._point));
