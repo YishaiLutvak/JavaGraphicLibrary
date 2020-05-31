4a35987651250e8264cc74144e569da548634f24
User <ylutvak@gmail.com>
2020-05-31 15:56:45 +0300

    finish stage 7 31/05/2020 16:00
    Signed-off-by: User <ylutvak@gmail.com>

--- UnitTests/UnitTests/ReflectionRefractionTests.java
+++ UnitTests/UnitTests/ReflectionRefractionTests.java
@@ -2,13 +2,10 @@ package UnitTests;
 
 import static org.junit.Assert.*;
 
-import geometries.Plane;
-import geometries.Polygon;
+import geometries.*;
 import org.junit.Test;
 
 import elements.*;
-import geometries.Sphere;
-import geometries.Triangle;
 import primitives.*;
 import renderer.*;
 import scene.Scene;
@@ -112,7 +109,42 @@ public class ReflectionRefractionTests {
      *  producing partial shadow
      */
     @Test
-    public void trianglesTransparentSphere2() {
+    public void trianglesTransparentCylinder() {
+        Scene scene = new Scene("Test scene");
+        scene.setCamera(new Camera(new Point3D(0, 0, -1000), new Vector(0, 0, 1), new Vector(0, -1, 0)));
+        scene.setDistance(1000);
+        scene.setBackground(Color.BLACK);
+        scene.setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.15));
+
+        scene.addGeometries( //
+                new Triangle(Color.BLACK, new Material(0.5, 0.5, 60), //
+                        new Point3D(-150, 150, 115), new Point3D(150, 150, 135), new Point3D(75, -75, 150)), //
+                new Triangle(Color.BLACK, new Material(0.2, 0.8, 60,0,0.8), //
+                        new Point3D(-150, 150, 115), new Point3D(-70, -70, 123), new Point3D(75, -75, 150)), //
+                new Cylinder(new Color(java.awt.Color.BLUE), new Material(0.2, 0.2, 30, 0.6, 0), 30, // )
+                        new Ray(new Point3D(55, -35, 50),new Vector(1,0,-0.25)),30),
+                new Sphere(Color.BLACK, new Material(0.2, 0.8, 30, 0, 0.8), // )
+                        30, new Point3D(-50, -50, 50)),
+                new Sphere(new Color(25,0,0), new Material(0, 0, 30, 1, 0), // )
+                        50, new Point3D(-50, -50, 50))
+        );
+
+        scene.addLights(new SpotLight(new Color(700, 400, 400), //
+                new Point3D(70, -50, -20), new Vector(0, 0, 1), 1, 4E-5, 2E-7));
+
+        ImageWriter imageWriter = new ImageWriter("Cylinder shadow transparency", 200, 200, 600, 600);
+        Render render = new Render(imageWriter, scene);
+
+        render.renderImage();
+        render.writeToImage();
+    }
+
+    /**
+     * Produce a picture of a two triangles lighted by a spot light with a partially transparent Sphere
+     *  producing partial shadow
+     */
+    @Test
+    public void myPicture() {
         Scene scene = new Scene("Test scene");
         scene.setCamera(new Camera(new Point3D(-300, -100, -300), new Vector(1, 0.2, 1), new Vector(1, -10, 1)));
         scene.setDistance(1000);
