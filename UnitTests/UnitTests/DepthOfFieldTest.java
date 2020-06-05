7dbcdd301d36a5e67764622019a816eface9a34b
User <ylutvak@gmail.com>
2020-06-05 10:49:45 +0300

    start stage 8 (7)
    Signed-off-by: User <ylutvak@gmail.com>

---
+++ UnitTests/UnitTests/DepthOfFieldTest.java
@@ -0,0 +1,46 @@
+package UnitTests;
+
+import elements.AmbientLight;
+import elements.Camera;
+import elements.SpotLight;
+import geometries.Sphere;
+import geometries.Triangle;
+import org.junit.Test;
+import primitives.Color;
+import primitives.Material;
+import primitives.Point3D;
+import primitives.Vector;
+import renderer.ImageWriter;
+import renderer.Render;
+import scene.Scene;
+
+import static org.junit.Assert.*;
+
+public class DepthOfFieldTest {
+
+    @Test
+    public void trianglesTransparentSphere() {
+        Scene scene = new Scene("Test scene");
+        scene.setCamera(new Camera(new Point3D(0, 0, -1000), new Vector(0, 0, 1), new Vector(0, -1, 0),1020,2,5,true));
+        scene.setDistance(1000);
+        scene.setBackground(Color.BLACK);
+        scene.setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.15));
+
+        scene.addGeometries( //
+                new Triangle(Color.BLACK, new Material(0.5, 0.5, 60), //
+                        new Point3D(-150, 150, 115), new Point3D(150, 150, 135), new Point3D(75, -75, 150)), //
+                new Triangle(Color.BLACK, new Material(0.5, 0.5, 60), //
+                        new Point3D(-150, 150, 115), new Point3D(-70, -70, 140), new Point3D(75, -75, 150)), //
+                new Sphere(new Color(java.awt.Color.BLUE), new Material(0.2, 0.2, 30, 0.6, 0), // )
+                        30, new Point3D(60, -50, 50)));
+
+        scene.addLights(new SpotLight(new Color(700, 400, 400), //
+                new Point3D(60, -50, 0), new Vector(0, 0, 1), 1, 4E-5, 2E-7));
+
+        ImageWriter imageWriter = new ImageWriter("depthOfFieldTest", 200, 200, 600, 600);
+        Render render = new Render(imageWriter, scene);
+
+        render.renderImage();
+        render.writeToImage();
+    }
+}
