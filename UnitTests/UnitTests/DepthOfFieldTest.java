4ef6102963153819507552911a2e748ee7eea2ce
User <ylutvak@gmail.com>
2020-06-08 14:02:57 +0300

    start stage 8 start scene
    Signed-off-by: User <ylutvak@gmail.com>

--- UnitTests/UnitTests/DepthOfFieldTest.java
+++ UnitTests/UnitTests/DepthOfFieldTest.java
@@ -72,36 +72,66 @@ public class DepthOfFieldTest {
     @Test
     public void depthOfFieldFullImage() {
         Scene scene = new Scene("Test scene");
-        scene.setCamera(new Camera(new Point3D(0, -1000, -1000), new Vector(0, 1, 1), new Vector(0, -1, 1),
-                1050, 12, 9, false));
+        scene.setCamera(new Camera(new Point3D(0, -120, -1000), new Vector(0, 0.05, 1), new Vector(0, -1, 0.05),
+                7000, 8, 5, true));
         scene.setDistance(1000);
         scene.setBackground(Color.BLACK);
         scene.setAmbientLight(new AmbientLight(Color.BLACK, 0.15));
 
+        Plane myPlane =  new Plane(new Color(25,0,51), new Material(0.1, 0, 60,0,0.9),
+                new Point3D(0,50,0),new Vector(0,1,0));
+        Tube MyTube = new Tube(new Color(150,150,150), new Material(0.5, 0.5, 60,0,0),//
+                100,new Ray(new Point3D(0,-50,500000),new Vector(1,0,0)));
+
         Geometries table  = new Geometries(
                 //plate of the table
-                new Polygon(new Color(new Color(java.awt.Color.GREEN)), new Material(0.5, 0.5, 60,0.5,0), //
-                        new Point3D(-100,0,-100), new Point3D(100,0,-100), new Point3D(100,0,100),//
-                        new Point3D(-100,0,100)),
+                /*new Polygon(new Color(76,0,153), new Material(0.5, 0.5, 60,0,0), //
+                        new Point3D(-50,0,100), new Point3D(50,0,100), new Point3D(50,0,150),//
+                        new Point3D(-50,0,150)),*/
+                new Cylinder(new Color(153,76,0), new Material(0.5, 0.5, 60,0.5,0),//
+                        65,new Ray(new Point3D(0,0,175),new Vector(0,1,0)), 2),
 
                 //legs of the table
-                new Cylinder(new Color(new Color(java.awt.Color.GREEN)), new Material(0.5, 0.5, 60,0.5,0),//
-                        10,new Ray(new Point3D(80,0,-80),new Vector(0,1,0)), 50),
-                new Cylinder(new Color(new Color(java.awt.Color.GREEN)), new Material(0.5, 0.5, 60,0.5,0),//
-                        10,new Ray(new Point3D(-80,0,-80),new Vector(0,1,0)), 50),
-                new Cylinder(new Color(new Color(java.awt.Color.GREEN)), new Material(0.5, 0.5, 60,0.5,0),//
-                        10,new Ray(new Point3D(80,0,80),new Vector(0,1,0)), 50),
-                new Cylinder(new Color(new Color(java.awt.Color.GREEN)), new Material(0.5, 0.5, 60,0.5,0),//
-                        10,new Ray(new Point3D(-80,0,80),new Vector(0,1,0)), 50));
+                new Cylinder(new Color(153,76,0), new Material(0.5, 0.5, 60,0.5,0),//
+                        1,new Ray(new Point3D(41,0,160),new Vector(0,1,0)), 60),
+                new Cylinder(new Color(153,76,0), new Material(0.5, 0.5, 60,0.5,0),//
+                        1,new Ray(new Point3D(-41,0,160),new Vector(0,1,0)), 60),
+                new Cylinder(new Color(153,76,0), new Material(0.5, 0.5, 60,0.5,0),//
+                        1,new Ray(new Point3D(39,0,190),new Vector(0,1,0)), 60),
+                new Cylinder(new Color(153,76,0), new Material(0.5, 0.5, 60,0.5,0),//
+                        1,new Ray(new Point3D(-39,0,190),new Vector(0,1,0)), 60));
 
-        Geometries bottle = new Geometries(new Cylinder(new Color(new Color(java.awt.Color.RED)), new Material(0.5, 0.5, 60,0.5,0),
-                10,new Ray(new Point3D(80,0,80),new Vector(0,-1,0)), 25),
-                new Cylinder(new Color(new Color(java.awt.Color.RED)), new Material(0.5, 0.5, 60,0.5,0),
-                        2,new Ray(new Point3D(80,-35,80),new Vector(0,-1,0)), 10),
-                new Sphere(new Color(new Color(java.awt.Color.RED)), new Material(0.5, 0.5, 60,0.5,0),
-                        10,new Point3D(80,-25,80)));
+        Geometries bottle = new Geometries(
+                new Cylinder(new Color(153,0,0), new Material(0.5, 0.5, 60,0.5,0),
+                10,new Ray(new Point3D(10,0,175),new Vector(0,-1,0)), 25),
+                new Cylinder(new Color(153,0,0), new Material(0.5, 0.5, 60,0.5,0),
+                        2,new Ray(new Point3D(10,-35,175),new Vector(0,-1,0)), 10),
+                new Sphere(new Color(153,0,0), new Material(0.5, 0.5, 60,0.5,0),
+                        10,new Point3D(10,-25,175)));
 
-        scene.addGeometries(table, bottle);
+        Geometries KiddushCup = new Geometries(
+                new Cylinder(new Color(64,64,64), new Material(0.5, 0.5, 60,0.5,0),
+                6,new Ray(new Point3D(-15,0,175),new Vector(0,-1,0)), 2),
+                new Cylinder(new Color(64,64,64), new Material(0.5, 0.5, 60,0.5,0),
+                        1,new Ray(new Point3D(-15,-2,175),new Vector(0,-1,0)), 5),
+                new Cylinder(new Color(64,64,64), new Material(0.5, 0.5, 60,0.5,0),
+                        6,new Ray(new Point3D(-15,-13,175),new Vector(0,-1,0)), 10),
+                new Sphere(new Color(64,64,64), new Material(0.5, 0.5, 60,0.5,0),
+                        6,new Point3D(-15,-13,175)));
+
+        Geometries balls = new Geometries(
+                new Sphere(new Color(120,0,0), new Material(0, 0, 0,0,1),
+                        150,new Point3D(0,-100,6000))/*,
+                new Sphere(new Color(0,0,150), new Material(0.2, 0.3, 60,0.0,0.5),
+                        20,new Point3D(-65,30,80))*/);
+
+        Geometries cylinders = new Geometries(
+                new Cylinder(new Color(53,0,26), new Material(0.2, 0.1, 60,0.7,0),
+                        10,new Ray(new Point3D(-100,-50,250),new Vector(0,-1,-0.3)), 40)
+                /*,new Cylinder(new Color(64,64,64), new Material(0.5, 0.5, 60,0.5,0),
+                        30,new Ray(new Point3D(-15,-13,400),new Vector(0,-1,0)), 10)*/);
+
+        scene.addGeometries(table, bottle, myPlane, KiddushCup , balls, /*cylinders,*/ MyTube);
 
 
         scene.addLights(new SpotLight(new Color(700, 400, 400), //
